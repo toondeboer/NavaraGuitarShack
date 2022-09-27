dab6a3c091827fdc59e76c9229206cfc7186612c
Toon de Boer <toondeboer@Toons-MacBook-Pro.local>
2022-09-27 18:50:22 +0200

    add test for not selling product

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const processSale = (getProduct, getProductRestockLevel, sendAlert, productId, quantity) => {
+const processSale = (
+  getProduct,
+  getProductRestockLevel,
+  sendAlert,
+  productId,
+  quantity
+) => {
+  if (getProduct().stock - quantity <= getProductRestockLevel()) {
     sendAlert("Please order more of product 811");
+  }
 };
- 
+
 module.exports = processSale;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,33 @@
 const assert = require("assert");
 const processSale = require("../src/index");
 
+const getProductRestockLevel = () => {
+  return 24;
+};
 describe("Product sold - alert", () => {
-    it(`send alert when product is equal to restock level`, () => {
-        let alertMessage = undefined;
-        const getProduct = () => {return {productId: 811, stock: 25 }};
-        const getProductRestockLevel = () => {return 24};
-        const sendAlert = (message) => {alertMessage = message};
-        processSale(getProduct, getProductRestockLevel, sendAlert, 811, 1);
-        assert.strictEqual(alertMessage, "Please order more of product 811");
-    })
-})
+  it(`send alert when product is equal to restock level`, () => {
+    let alertMessage = undefined;
+    const getProduct = () => {
+      return { productId: 811, stock: 25 };
+    };
+    const sendAlert = (message) => {
+      alertMessage = message;
+    };
+    processSale(getProduct, getProductRestockLevel, sendAlert, 811, 1);
+    assert.strictEqual(alertMessage, "Please order more of product 811");
+  });
+});
+
+describe("Product sold - no alert", () => {
+  it(`not send alert when product is more than restock level`, () => {
+    let alertMessage = undefined;
+    const getProduct = () => {
+      return { productId: 811, stock: 26 };
+    };
+    const sendAlert = (message) => {
+      alertMessage = message;
+    };
+    processSale(getProduct, getProductRestockLevel, sendAlert, 811, 1);
+    assert.strictEqual(alertMessage, undefined);
+  });
+});
